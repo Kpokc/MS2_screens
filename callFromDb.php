c7492625cf99dcd184d87671c1363799907c4d23
Kpokc <pavmakarovs@gmail.com>
2021-04-26 13:49:34 +0100

    callFromDb.php - sql query functions were written

--- callFromDb.php
+++ callFromDb.php
@@ -1,54 +1,153 @@
 <?php
+include('login.php');
+if(!isset($_SESSION['login_user']))
+{
+    header("location: index.php"); // Redirecting To Home Page
+}
+else {
+    
+    
+    
+    function callPicksReceipts($user, $pass){
+	            
+            	$hostname="localhost";
+            	$username=$user;
+            	$password=$pass;
+            	$dbname="pick_test";
+            	$usertable="message";
 
-$hostname = "localhost";
-$username = $user;
-$password = $password;
-$dbname = "pick_test";
+	            //Connect To Database
+            	mysqli_connect($hostname,$username, $password) or die ("<html><script language='JavaScript'>alert('Unable to connect to database! Please try again later.')</script></html>");
+            	mysqli_select_db($dbname);
+	
+            	// Check If Record Exists
+            	$conn = new mysqli($hostname, $username, $password, $dbname);
+                    // Check connection
+                    if ($conn->connect_error) {
+                      die("Connection failed: " . $conn->connect_error);
+                    }
 
-mysqli_connect($hostname,$username,$password) or die ("<script>alert('Unable to connect')</script>");
-mysqli_select_db($dbname);
+                $sql = "SELECT * FROM message 
+                        WHERE job = 'pick'
+                        OR job = 'receipt'
+                        ORDER BY u_id ASC";
+                    $result = $conn->query($sql);
+        
+                
+                if ($result->num_rows > 0) {
+                  // Output Data of Each Row
+                  while($row = $result->fetch_assoc()) {
+                    echo "<li class='msg-card'>
 
-$conn = new mysqli($hostname, $username, $password, $dbname);
+                            <div class='card-header'>
+                                <div class='card-header-inner'>
+                                    <h4>".$row["job"]." : UD</h4>
+                                </div>
+                                <div class='card-header-inner'>
+                                    <h4>".$row["u_id"]."</h4>
+                                </div>
+                            </div>
 
-    if ($conn->connect_error){
-        die ("<script>alert('Connection failed')</script>");
+                            <div class='card-body'>
+                                <div class='body-section-left'>
+                                    <div class='card-body-vendor'>
+                                        ".$row["vendor"]."
+                                    </div>
+                                    <div class='card-body-inner'>
+                                        ".$row["time"]."
+                                    </div>
+                                    <div class='card-body-inner'>
+                                        ID: ".$row["pick_id"]." 
+                                    </div>
+                                </div>
+
+                                <div class='body-section-right'>
+                                    <div class='messege'>
+                                        ".$row["msg"]."
+                                    </div>
+                                </div>
+                            </div>
+
+                        </li>";
+                  }
+                } else {
+                  echo "<div class='zeroResult'><h3>0 results</h3></div>";
+                }
+                $conn->close();
+                
+                return $html;
+            
     }
+    
+    function callDeliveryCollection($user, $pass){
+         
+            	$hostname="localhost";
+            	$username=$user;
+            	$password=$pass;
+            	$dbname="pick_test";
+            	$usertable="message";
 
+	            //Connect To Database
+            	mysqli_connect($hostname,$username, $password) or die ("<html><script language='JavaScript'>alert('Unable to connect to database! Please try again later.')</script></html>");
+            	mysqli_select_db($dbname);
+	
+            	// Check If Record Exists
+            	$conn = new mysqli($hostname, $username, $password, $dbname);
+                    // Check connection
+                    if ($conn->connect_error) {
+                      die("Connection failed: " . $conn->connect_error);
+                    }
 
-    $sql = "SELECT * FROM message";
-    $result = $conn->query($sql);
+                $sql = "SELECT * FROM message 
+                        WHERE job = 'delivery'
+                        OR job = 'collection'
+                        OR job = 'transfer'
+                        ORDER BY id ASC";
+                    $result = $conn->query($sql);
+        
+                if ($result->num_rows > 0) {
+                  // Output Data of Each Row
+                  while($row = $result->fetch_assoc()) {
+                    echo "<li class='msg-card'>
 
-    if ($result->num_rows > 0) {
-        while($row = $result->fetch_assoc()){
-            echo "<li class='msg-card'>
-                    <div class='card-header'>
-                        <div class='card-header-inner'>
-                            <h4>".$row["job"]." : UD</h4>
-                        </div>
-                        <div class='card-header-inner'>
-                            <h4>".$row["u_id"]."</h4>
-                        </div>
-                    </div>
-                    <div class='card-body'>
-                        <div class='body-section-left'>
-                            <div class='card-body-vendor'>
-                                ".$row["vendor"]."
+                            <div class='card-header'>
+                                <div class='card-header-inner'>
+                                    <h4>".$row["job"]." : UD</h4>
+                                </div>
+                                <div class='card-header-inner'>
+                                    <h4>".$row["u_id"]."</h4>
+                                </div>
                             </div>
-                            <div class='card-body-inner'>
-                                ".$row["time"]."
-                            </div>
-                            <div class='card-body-inner'>
-                                ID: ".$row["pick_id"]." 
-                            </div>
-                        </div>
-                        <div class='body-section-right'>
-                            <div class='messege'>
-                                ".$row["msg"]."
-                            </div>
-                        </div>
-                    </div>
-                </li>";
-        }
-    };
 
+                            <div class='card-body'>
+                                <div class='body-section-left'>
+                                    <div class='card-body-vendor'>
+                                        ".$row["vendor"]."
+                                    </div>
+                                    <div class='card-body-inner'>
+                                        ".$row["time"]."
+                                    </div>
+                                    <div class='card-body-inner'>
+                                        ID: ".$row["pick_id"]." 
+                                    </div>
+                                </div>
+
+                                <div class='body-section-right'>
+                                    <div class='messege'>
+                                        ".$row["msg"]."
+                                    </div>
+                                </div>
+                            </div>
+                            
+                        </li>";
+                  }
+                } else {
+                  echo "<div class='zeroResult'><h3>0 results</h3></div>";
+                }
+                $conn->close();
+                
+                return $html;
+            
+    }
+}
 ?>
