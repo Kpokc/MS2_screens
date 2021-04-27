ca8cb0bc37e1f18f709ac8a7f922f535328184e8
Kpokc <pavmakarovs@gmail.com>
2021-04-27 10:17:03 +0100

    Current time clock added to screen.php

---
+++ assets/js/main.js
@@ -0,0 +1,28 @@
+var today = new Date();
+var time = today.getHours() + ":" + today.getMinutes() + ":" + today.getSeconds();
+            
+timer();
+
+function timer(){
+    var currentTime = new Date()
+    var hours = currentTime.getHours()
+    var minutes = currentTime.getMinutes()
+    var sec = currentTime.getSeconds()
+                
+    if (minutes < 10){
+        minutes = "0" + minutes
+    }
+    if (sec < 10){
+        sec = "0" + sec
+    }
+                
+    var t_str = hours + ":" + minutes + ":" + sec + " ";
+    if(hours > 11){
+        t_str;
+    } else {
+        t_str;
+    }
+                
+    document.getElementById('time_span').innerHTML = t_str;
+        setTimeout(timer,1000);
+}
