82950434ce3d7b7363da5f2541b11ae1ae518ef6
Kpokc <pavmakarovs@gmail.com>
2021-04-28 20:05:25 +0100

    Update function - code cleaned & comented

--- assets/js/main.js
+++ assets/js/main.js
@@ -155,24 +155,26 @@ $("li").dblclick(function(){
 
 // Update message input field listener
 $("#upload-bttn").click(function(){
+    // Wait till form loads
     $("#updateRow").ready(function(){
+        // Listen for digits
         $("#unique_id").on("input", function(){
+            
+            // clean all fields
+            clean();
 
-            $(".cardToDeleteClone").remove();
-
-            $("#urgent_upd").val("");
-            $("#select_upd").val("");
-            $("#vendor_upd").val("");
-            $("#pick_id_upd").val("");
-            $("#message_upd").val("");
-
-            //console.log($("#unique_id").val());
+            // Get Uniq Id from input field
             var cardId = $("#unique_id").val();
+            // Array of all "li" in DOM
             var arrayli = $("li");
+            // Loop throughout all "li" looking for "id" to equals "cardId"
             for (i = 0; i < arrayli.length; i++){
                 if($(arrayli[i]).attr("id") === cardId){
+                    // Get all text from "li"
                     let k = $(arrayli[i]).text();
+                    // Replace all whitespace in string "k" with single space
                     k = k.replace(/\s+/g, ' ');
+                    // Split "k" into array
                     let textArray = k.split(" ");
                     $("#urgent_upd").val(textArray[1]);
                     $("#select_upd").val(textArray[2]);
@@ -188,31 +190,33 @@ $("#upload-bttn").click(function(){
                     cardToDelete.innerWidth(cardClientWidth).css("margin","0 auto").addClass("cardToDeleteClone").css("box-shadow","0 3px 10px 5px #1a73e8").css("color","#f5f5f5");
                     // Add card to modal window
                     $("#exampleModalUpdate").append(cardToDelete);
-
-                    console.log(textArray);
                 }
             }
         });
+        // If "input" field changed - change mark from "Current" to "Changed" 
         $("input").on("input", function(){
             $(this).next().css("color","red").text("Changed");
         });
+        // If "select" field changed - change mark from "Current" to "Changed"
+        $("select").on("change", function(){
+            $(this).next().css("color","red").text("Changed");
+        });
+        // If "texarea" field changed - change mark from "Current" to "Changed"
+        //https://www.codegrepper.com/c
+        $('#message_upd').on('input propertychange paste', function() {
+            $(this).prev().css("color","red").text("Changed");
+        });
 
+        // Listen if procedure was aborted 
         $("#exampleModalUpdate").click(function(e){
             // Get clicked target "id" value
             let tr = e.target;
             // If value equals any of "close" button or modal window it self
                 if($(tr).attr("id") === "exampleModalUpdate" || $(tr).attr("id") === "closeBttn"){
-                    //remove div with the table
-                    $(".cardToDeleteClone").remove();
-
-                    $("aside").css("color","black").text("Current");
-                    
+                    // clean unique id input field
                     $("#unique_id").val("");
-                    $("#urgent_upd").val("");
-                    $("#select_upd").val("");
-                    $("#vendor_upd").val("");
-                    $("#pick_id_upd").val("");
-                    $("#message_upd").val("");
+                    // clean all fields
+                    clean();
                 }
         });
 
@@ -294,4 +298,18 @@ function messageNotSent(){
        $(".not-ok-div").css("display","none");
        $(".modal-form").css("display","block");
    }, 1500);
+}
+
+function clean(){
+    // Remove "appended" card from modal bottom
+    $(".cardToDeleteClone").remove();
+    // All "Changed" marks back to "Current" 
+    $("aside").css("color","black").text("Current");
+    
+    // Empty all fields 
+    $("#urgent_upd").val("");
+    $("#select_upd").val("");
+    $("#vendor_upd").val("");
+    $("#pick_id_upd").val("");
+    $("#message_upd").val("");
 }
