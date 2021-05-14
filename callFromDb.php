f6f96a7f83107c3d114101ec47275381a4c1f7de
Kpokc <pavmakarovs@gmail.com>
2021-05-14 09:39:54 +0100

    Heroku - screen.php to select

--- callFromDb.php
+++ callFromDb.php
@@ -12,10 +12,10 @@ else {
     function callPicksReceipts(){
 	            
                 //DB variables
-            	$hostname="localhost";
+            	$hostname="f80b6byii2vwv8cx.chr7pe7iynqr.eu-west-1.rds.amazonaws.com";
             	$username = $_SESSION['login_user'];
                 $password = $_SESSION['login_pass'];
-            	$dbname="pick_test";
+            	$dbname="isfxtxolqppnqivp";
 
 	            //Connect To Database
             	mysqli_connect($hostname,$username, $password) or die ("<html><script language='JavaScript'>alert('Unable to connect to database! Please try again later.')</script></html>");
