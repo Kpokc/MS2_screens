8091a9bee09f7f23839f22a3bd3e5450e0262e54
Kpokc <pavmakarovs@gmail.com>
2021-05-14 09:16:56 +0100

    LocalHost changed

--- login.php
+++ login.php
@@ -6,9 +6,9 @@ if (isset($_POST['submit'])) {
     // Define $username and $password
     $username = $_POST['username'];
     $password = $_POST['password'];
-    
+    $localHost = "f80b6byii2vwv8cx.chr7pe7iynqr.eu-west-1.rds.amazonaws.com";
     // mysqli_connect() function opens a new connection to the MySQL server.
-    $conn = mysqli_connect("localhost", $username, $password, "pick_test");
+    $conn = mysqli_connect($localHost, $username, $password, "pick_test");
     // SQL query to fetch information of registerd users and finds user match.
     $query = "SELECT username, password FROM user WHERE username='$username' AND password='$password'";
     
